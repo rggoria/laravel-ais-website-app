a4ee0ade43ba56aaafa20e3c64e710547e011e76
Ram Emerson Goria <goria.reg@gmail.com>
2024-09-30 12:41:38 +0800

    Navbar mega menu

---
+++ resources/views/partials/mega.php
@@ -0,0 +1,75 @@
+<style>
+    .megamenu {
+        position: static;
+    }
+  .megamenu .dropdown-menu {
+        background: none;
+        border: none;
+        width: 100%;
+    }
+</style>
+<nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
+    <div class="container-fluid">
+        <a href="#" class="navbar-brand fw-bold d-block d-lg-none">MegaMenu</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarSupportedContent" class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown megamenu m-0"><a id="megamneu" href="#" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" class="nav-link dropdown-toggle fw-bold text-uppercase dropdown-toggle">Mega Menu</a>
+            <div aria-labelledby="dropdownMenuButton1" class="dropdown-menu border-0 p-0 m-0">
+                <div class="container-fluid p-0">
+                <div class="row bg-white rounded-0 m-0 shadow-sm">
+                    <div class="col-lg-7 col-xl-8">
+                    <div class="p-4">
+                        <div class="row">
+                        <div class="col-lg-6 mb-4">
+                            <h6 class="fw-bold text-uppercase">MegaMenu heading</h6>
+                            <ul class="list-unstyled">
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0">Unique Features</a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Image Responsive</a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Auto Carousel</a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Newsletter Form</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6 mb-4">
+                            <h6 class="fw-bold text-uppercase">MegaMenu heading</h6>
+                            <ul class="list-unstyled">
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Unique Features</a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Image Responsive</a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Auto Carousel</a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Newsletter Form</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6 mb-4">
+                            <h6 class="fw-bold text-uppercase">MegaMenu heading</h6>
+                            <ul class="list-unstyled">
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Unique Features</a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Image Responsive</a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Auto Carousel</a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Newsletter Form</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6 mb-4">
+                            <h6 class="fw-bold text-uppercase">MegaMenu heading</h6>
+                            <ul class="list-unstyled">
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Unique Features</a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Image Responsive</a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Auto Carousel</a></li>
+                            <li class="nav-item"><a href="" class="nav-link text-small pb-0 ">Newsletter Form</a></li>
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-lg-5 col-xl-4 px-0 d-none d-lg-block" style="background: center center url(https://therichpost.com/wp-content/uploads/2021/11/mega.png)no-repeat; background-size: cover;"></div><br>              </div>
+                </div>
+            </div>
+            </li>
+            <li class="nav-item"><a href="" class="nav-link fw-bold text-uppercase">About</a></li>
+            <li class="nav-item"><a href="" class="nav-link fw-bold text-uppercase">Services</a></li>
+            <li class="nav-item"><a href="" class="nav-link fw-bold text-uppercase">Contact</a></li>
+        </ul>
+        </div>
+    </div>
+</nav>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,14 @@
-<nav class="navbar navbar-expand-lg">
+<style>
+    .mega-menu {
+        position: static;
+    }
+    .mega-menu .dropdown-menu {
+        background: none;
+        border: none;
+        width: 100%;
+    }
+</style>
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="#">
             <img src="{{ asset('asset/images/homepage/logo.png') }}" alt="Logo" width="50" height="44" class="d-inline-block align-text-top">
@@ -9,34 +19,167 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home Page</a>
+                    <a class="nav-link active" aria-current="page" href="#!">Home Page</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Services</a>
+                    <a class="nav-link" aria-current="page" href="#!">Services</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">About Us</a>
-                </li>    
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link" aria-current="page" href="#!">About Us</a>
+                </li>
+                <li class="nav-item dropdown mega-menu">
+                    <a class="nav-link dropdown-toggle" href="#!" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Pricing
                     </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Employment pass</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
+                    <div class="dropdown-menu border-0 p-0 m-0">
+                        <div class="container-fluid py-4 shadow-sm bg-white">
+                            <div class="container">
+                                <div class="row rounded-0 m-0">
+                                    <div class="col-lg-3">
+                                        <ul class="list-unstyled">
+                                            <li class="nav-item">
+                                                <a href="#!" class="nav-link d-flex align-items-start">
+                                                    <i class="fas fa-cube me-2 fs-6 mt-1 align-self-start"></i>
+                                                    <span class="fw-bold">
+                                                        Employment pass
+                                                        <br>
+                                                        <span class="text-muted">
+                                                            S$999 - Best selling item
+                                                        </span>
+                                                    </span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <ul class="list-unstyled">
+                                            <li class="nav-item">
+                                                <a href="#!" class="nav-link d-flex align-items-start">
+                                                    <i class="fas fa-cube me-2 fs-6 mt-1 align-self-start"></i>
+                                                    <span class="fw-bold">
+                                                        Page two
+                                                        <br>
+                                                        <span class="text-muted">
+                                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit
+                                                        </span>
+                                                    </span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <ul class="list-unstyled">
+                                            <li class="nav-item">
+                                                <a href="#!" class="nav-link d-flex align-items-start">
+                                                    <i class="fas fa-cube me-2 fs-6 mt-1 align-self-start"></i>
+                                                    <span class="fw-bold">
+                                                        Page three
+                                                        <br>
+                                                        <span class="text-muted">
+                                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit
+                                                        </span>
+                                                    </span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <ul class="list-unstyled">
+                                            <li class="nav-item">
+                                                <a href="#!" class="nav-link d-flex align-items-start">
+                                                    <i class="fas fa-cube me-2 fs-6 mt-1 align-self-start"></i>
+                                                    <span class="fw-bold">
+                                                        Page four
+                                                        <br>
+                                                        <span class="text-muted">
+                                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit
+                                                        </span>
+                                                    </span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <li class="nav-item dropdown mega-menu">
+                    <a class="nav-link dropdown-toggle" href="#!" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Contact Us
                     </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Consultation</a></li>
-                        <li><a class="dropdown-item" href="#">Testimonials</a></li>
-                        <li><a class="dropdown-item" href="#">Resources</a></li>
-                        <li><a class="dropdown-item" href="#">Contact Us</a></li>
-                    </ul>
+                    <div class="dropdown-menu border-0 p-0 m-0">
+                        <div class="container-fluid py-4 shadow-sm bg-white">
+                            <div class="container">
+                                <div class="row rounded-0 m-0">
+                                    <div class="col-lg-3">
+                                        <ul class="list-unstyled">
+                                            <li class="nav-item">
+                                                <a href="#!" class="nav-link d-flex align-items-start">
+                                                    <i class="fas fa-cube me-2 fs-6 mt-1 align-self-start"></i>
+                                                    <span class="fw-bold">
+                                                        Consultation
+                                                        <br>
+                                                        <span class="text-muted">
+                                                            Get personalized immigration advice today.
+                                                        </span>
+                                                    </span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <ul class="list-unstyled">
+                                            <li class="nav-item">
+                                                <a href="#!" class="nav-link d-flex align-items-start">
+                                                    <i class="fas fa-cube me-2 fs-6 mt-1 align-self-start"></i>
+                                                    <span class="fw-bold">
+                                                        Testimonials
+                                                        <br>
+                                                        <span class="text-muted">
+                                                            Hear from out satisfied clients.
+                                                        </span>
+                                                    </span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <ul class="list-unstyled">
+                                            <li class="nav-item">
+                                                <a href="#!" class="nav-link d-flex align-items-start">
+                                                    <i class="fas fa-cube me-2 fs-6 mt-1 align-self-start"></i>
+                                                    <span class="fw-bold">
+                                                        Resources
+                                                        <br>
+                                                        <span class="text-muted">
+                                                            Access helpful immigration guides and tips
+                                                        </span>
+                                                    </span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <ul class="list-unstyled">
+                                            <li class="nav-item">
+                                                <a href="#!" class="nav-link d-flex align-items-start">
+                                                    <i class="fas fa-cube me-2 fs-6 mt-1 align-self-start"></i>
+                                                    <span class="fw-bold">
+                                                        Contact Us
+                                                        <br>
+                                                        <span class="text-muted">
+                                                            Reach out for any inquiries or support.
+                                                        </span>
+                                                    </span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </li>
             </ul>
             <ul class="navbar-nav d-flex gap-3">
@@ -47,6 +190,6 @@
                     <a class="btn btn-dark text-light" href="#">Order Now</a>
                 </li>
             </ul>
-        </div>        
+        </div>
     </div>
 </nav>
